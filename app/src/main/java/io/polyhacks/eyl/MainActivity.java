e58014a82ecd902fcb1adda1f48d5285faa192f4
Guillaume RICARD <guillaume.ricard@mail.com>
2023-02-04 16:24:11 -0500

    camera: Add camera preview and photo capture
    Use Android CameraX APIs to show a live preview of the camera feed and capture
    an image file on button press.

--- app/src/main/java/io/polyhacks/eyl/MainActivity.java
+++ app/src/main/java/io/polyhacks/eyl/MainActivity.java
@@ -1,76 +1,188 @@
 package io.polyhacks.eyl;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.google.common.util.concurrent.ListenableFuture;
 import io.polyhacks.eyl.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+  private final String TAG = "eyl";
+  private final String FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS";
+  private final int REQUEST_CODE_PERMISSIONS = 10;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  private ActivityMainBinding viewBinding;
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+  private ImageCapture imageCapture;
 
-        setSupportActionBar(binding.toolbar);
+  private ExecutorService cameraExecutor;
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+    viewBinding = ActivityMainBinding.inflate(getLayoutInflater());
+    setContentView(viewBinding.getRoot());
+
+    if (allPermissionsGranted()) {
+      startCamera();
+    } else {
+      ArrayList<String> permissions = new ArrayList<>();
+      permissions.add(Manifest.permission.CAMERA);
+      permissions.add(Manifest.permission.RECORD_AUDIO);
+      if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+        permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+      }
+      ActivityCompat.requestPermissions(
+          this, permissions.toArray(new String[0]), REQUEST_CODE_PERMISSIONS);
+    }
+
+    viewBinding.imageCaptureButton.setOnClickListener(
+        (View view) -> {
+          takePhoto();
         });
+
+    cameraExecutor = Executors.newSingleThreadExecutor();
+  }
+
+  @Override
+  public void onRequestPermissionsResult(
+      int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    if (requestCode == REQUEST_CODE_PERMISSIONS) {
+      if (allPermissionsGranted()) {
+        startCamera();
+      } else {
+        Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
+        finish();
+      }
+    }
+  }
+
+  private void takePhoto() {
+    if (imageCapture == null) {
+      return;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    String name =
+        new SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis());
+    ContentValues contentValues = new ContentValues();
+    contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
+    contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+      contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/EYL");
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    ImageCapture.OutputFileOptions outputOptions =
+        new ImageCapture.OutputFileOptions.Builder(
+                getContentResolver(), MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+            .build();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    imageCapture.takePicture(
+        outputOptions,
+        ContextCompat.getMainExecutor(this),
+        new ImageCapture.OnImageSavedCallback() {
+          @Override
+          public void onImageSaved(@NonNull ImageCapture.OutputFileResults output) {
+            String msg = String.format("Photo capture succeeded: %s", output.getSavedUri());
+            Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
+            Log.d(TAG, msg);
+          }
 
-        return super.onOptionsItemSelected(item);
+          @Override
+          public void onError(@NonNull ImageCaptureException exception) {
+            Log.e(TAG, String.format("Photo capture failed: %s", exception.getMessage()));
+          }
+        });
+  }
+
+  private void startCamera() {
+    ListenableFuture<ProcessCameraProvider> cameraProviderFuture =
+        ProcessCameraProvider.getInstance(this);
+    cameraProviderFuture.addListener(
+        () -> {
+          ProcessCameraProvider cameraProvider;
+          try {
+            cameraProvider = cameraProviderFuture.get();
+          } catch (Exception e) {
+            return;
+          }
+
+          Preview preview = new Preview.Builder().build();
+          preview.setSurfaceProvider(viewBinding.viewFinder.getSurfaceProvider());
+
+          imageCapture = new ImageCapture.Builder().build();
+
+          ImageAnalysis imageAnalyzer = new ImageAnalysis.Builder().build();
+          imageAnalyzer.setAnalyzer(
+              cameraExecutor,
+              new ImageAnalysis.Analyzer() {
+                @Override
+                public void analyze(@NonNull ImageProxy image) {
+                  ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                  buffer.rewind();
+                  byte[] data = new byte[buffer.remaining()];
+                  buffer.get(data);
+
+                  int luma = (int) data[0];
+                  Log.d(TAG, String.format("First pixel: %d", luma));
+
+                  image.close();
+                }
+              });
+
+          try {
+            cameraProvider.unbindAll();
+
+            cameraProvider.bindToLifecycle(
+                this, CameraSelector.DEFAULT_BACK_CAMERA, preview, imageCapture, imageAnalyzer);
+          } catch (Exception exception) {
+            Log.e(TAG, "Use case binding failed", exception);
+          }
+        },
+        ContextCompat.getMainExecutor(this));
+  }
+
+  public boolean allPermissionsGranted() {
+    boolean result =
+        ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.CAMERA)
+            == PackageManager.PERMISSION_GRANTED;
+    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+      result &=
+          ContextCompat.checkSelfPermission(
+                  getBaseContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+              == PackageManager.PERMISSION_GRANTED;
     }
+    return result;
+  }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    cameraExecutor.shutdown();
+  }
 }
